3b72bbb04178e0c47048f541e46dd4211a5fabe2
Luiz Lopes <lg.oliveira.lopes@gmail.com>
2023-04-10 16:54:24 -0300

    fix frontend: add injectable

--- frontend/app/src/app/app.component.ts
+++ frontend/app/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component} from '@angular/core';
+import {Injectable} from '@angular/core';
  
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+})
+@Injectable({
+  providedIn: 'root'
 })
 export class AppComponent {
   title = 'clientes-app';
